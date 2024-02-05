3dbe7f34deec5bda809b2bd271abfa465352ff16
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-05 17:48:59 +0100

    Adding endpoint verify/:verificationToken.

--- modules/users/routes/users.js
+++ modules/users/routes/users.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import "../../auth/services/auth.strategy.js";
 import auth from "../../auth/middlewares/auth.js";
+import { verifyToken } from "../controllers/verify.js";
 import { uploadMiddleware } from "../middlewares/avatar.js";
 import { updateUser, updateAvatar } from "../controllers/update.js";
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.patch("/", auth, updateUser);
 router.patch("/avatars", auth, uploadMiddleware.single("avatar"), updateAvatar);
+router.get("/verify/:verificationToken", verifyToken);
 
 export default router;
