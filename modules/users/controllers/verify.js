b4107d7d6175a5832e7d015e177ba274ad735983
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-06 16:42:47 +0100

    Creating new function to send email with verification token.

--- modules/users/controllers/verify.js
+++ modules/users/controllers/verify.js
@@ -1,4 +1,5 @@
 import { User } from "../schemas/user.schema.js";
+import { sendEmail } from "../../shared/sevices/mail.js";
 
 export async function verifyToken(req, res) {
   try {
@@ -15,5 +16,29 @@ export async function verifyToken(req, res) {
     return res.status(200).json("Verification successful");
   } catch (e) {
     return res.status(500).json(e.message);
+  }
+}
+
+export async function verificationEmailRequest(req, res) {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(400).json("Missing requierd field email");
+    }
+    const data = await User.find({ email: email });
+    const user = data[0];
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+    if (user.verify) {
+      return res.status(400).json("Verification has already been passed");
+    }
+    await sendEmail({
+      to: user.email,
+      subject: "Verification request",
+      text: `Hi. You've recieved this email because there has been a request to send again verification email. Ignore this email if it wasn't you. Here it is and this is your verification endpoint - users/verify/${user.verificationToken}`,
+    });
+  } catch (e) {
+    res.status(500).json("Error occured", e.message);
   }
 }
