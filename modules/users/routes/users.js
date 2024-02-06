5dfadbd3cd15f58ff8aab4980cb2184deaa8a591
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-06 16:44:04 +0100

    Adding new endpoint.

--- modules/users/routes/users.js
+++ modules/users/routes/users.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import "../../auth/services/auth.strategy.js";
 import auth from "../../auth/middlewares/auth.js";
-import { verifyToken } from "../controllers/verify.js";
 import { uploadMiddleware } from "../middlewares/avatar.js";
 import { updateUser, updateAvatar } from "../controllers/update.js";
+import {
+  verifyToken,
+  verificationEmailRequest,
+} from "../controllers/verify.js";
 
 const router = Router();
 
 router.patch("/", auth, updateUser);
 router.patch("/avatars", auth, uploadMiddleware.single("avatar"), updateAvatar);
+router.post("/verify", verificationEmailRequest);
 router.get("/verify/:verificationToken", verifyToken);
 
 export default router;
